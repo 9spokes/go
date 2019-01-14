1ee2fdcd04a62205659fe00af81a0bfd9184b822
Mourad Kessas <mourad.kessas@9spokes.com>
2019-01-14 18:15:26 +1300

    Updated network/dial

--- network/dial.go
+++ network/dial.go
@@ -7,20 +7,24 @@ import (
 )
 
 // Dial attempts to connect to a TCP or UDP destination until timeout value is reached.
-func Dial(proto string, port int, timeout int) error {
+func Dial(proto string, dest string, timeout int) error {
 
+	ch := make(chan string, 1)
 	go func() {
-		conn, err := net.Dial("tcp", ":1123")
-		if err != nil {
-			time.Sleep(1000 * time.Second)
+		for {
+			_, err := net.Dial(proto, dest)
+			if err == nil {
+				ch <- "connected"
+				return
+			}
+			time.Sleep(1 * time.Second)
 		}
-		defer conn.Close()
 	}()
 
 	select {
 	case <-time.After(time.Duration(timeout) * time.Second):
-		return fmt.Errorf("Timeout connecting to %s/%d after %d seconds", proto, port, timeout)
-	default:
+		return fmt.Errorf("Timeout connecting to %s/%s after %d seconds", proto, dest, timeout)
+	case <-ch:
 		return nil
 	}
 }
