924c73ee0d362c13b3e9b91ba9cc0276afff268d
Reshad Contractor <reshad.contractor@9spokes.com>
2019-03-01 14:27:28 +1300

    converting to use map[string]interface{}

--- crypto/oauth1.go
+++ crypto/oauth1.go
@@ -8,13 +8,13 @@ import (
 )
 
 //Signer generates a signature for a xero request
-func Signer(auth map[string]string) (string, error) {
+func Signer(auth map[string]interface{}) (string, error) {
 
 	url := getKey(auth, "BaseURL")
 	secret := getKey(auth, "Secret")
 	method := getKey(auth, "Method")
 
-	sortedAuthString := func(Auth map[string]string) string {
+	sortedAuthString := func(Auth map[string]interface{}) string {
 		keys := make([]string, 0, len(Auth))
 		for K := range Auth {
 			keys = append(keys, K)
@@ -22,16 +22,16 @@ func Signer(auth map[string]string) (string, error) {
 		sort.Strings(keys)
 		var sortedAuthString strings.Builder
 		for _, K := range keys {
-			sortedAuthString.WriteString(K + "=" + misc.OauthEscape(Auth[K]) + "&")
+			sortedAuthString.WriteString(K + "=" + misc.OauthEscape(Auth[K].(string)) + "&")
 		}
 		return sortedAuthString.String()[:len(sortedAuthString.String())-1]
 	}(auth)
 
-	signature := Sign([]byte(strings.Join([]string{method, misc.OauthEscape(url), misc.OauthEscape(sortedAuthString)}, "&")), auth["oauth_signature_method"], secret)
+	signature := Sign([]byte(strings.Join([]string{method, misc.OauthEscape(url), misc.OauthEscape(sortedAuthString)}, "&")), auth["oauth_signature_method"].(string), secret)
 	return signature, nil
 }
 
-func getKey(input map[string]string, key string) string {
+func getKey(input map[string]interface{}, key string) string {
 
 	if _, ok := input[key]; !ok {
 		panic("Missing " + key)
@@ -39,5 +39,5 @@ func getKey(input map[string]string, key string) string {
 
 	ret := input[key]
 	delete(input, key)
-	return ret
+	return ret.(string)
 }
