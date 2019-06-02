1baba1da302dd72d7cfbe764fa25a918e9d07616
Mourad Kessas <mourad.kessas@9spokes.com>
2019-06-02 16:18:50 +1200

    Added http convenience methods

--- http/http.go
+++ http/http.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
@@ -82,4 +83,45 @@ func (request Request) http() (*Response, error) {
 	}
 
 	return &Response{resp.StatusCode, body}, nil
+}
+
+// ErrorResponse sends a standardised error message body to the caller
+// in the form of { "status": "err", "message": "<your error message>"}
+func ErrorResponse(w http.ResponseWriter, msg string, code int) {
+
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Method", "*")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("AAccess-Control-Request-Headers", "*")
+
+	response := struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+	}{"err", msg}
+
+	e, _ := json.Marshal(response)
+	w.WriteHeader(code)
+	w.Write(e)
+	return
+}
+
+// SuccessResponse sends a standardised success message body to the caller
+// in the form of { "status": "ok", "details": "<your data>"}
+func SuccessResponse(w http.ResponseWriter, data interface{}, code int) {
+
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Request-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Method", "*")
+
+	response := struct {
+		Status  string      `json:"status"`
+		Details interface{} `json:"details"`
+	}{"ok", data}
+
+	o, _ := json.Marshal(response)
+	w.Write(o)
+	return
 }
