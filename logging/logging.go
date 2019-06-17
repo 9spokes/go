74b06f8c4d6f2ca7281e8bb9517abc122782b2d8
Mourad Kessas <mourad.kessas@9spokes.com>
2019-06-17 19:36:03 +1200

    Added common logging

---
+++ logging/logging.go
@@ -0,0 +1,35 @@
+package logging
+
+import (
+	"os"
+	"strings"
+
+	"github.com/op/go-logging"
+)
+
+// New ceates a new 9 Spokes logger with a formatter, uses stdout as a backend.
+func New(category, level string) *logging.Logger {
+	logger := logging.MustGetLogger(category)
+
+	var format = logging.MustStringFormatter(
+		`%{color}%{time:15:04:05.000} %{level:.5s} %{shortfunc}() ▶ %{message}%{color:reset}`,
+	)
+	logging.SetBackend(logging.NewBackendFormatter(logging.NewLogBackend(os.Stdout, "", 0), format))
+
+	level = strings.ToUpper(level)
+	if level == "DEBUG" {
+		logging.SetLevel(logging.DEBUG, category)
+		return logger
+	}
+	if level == "ERROR" {
+		logging.SetLevel(logging.ERROR, category)
+		return logger
+	}
+	if level == "CRITICAL" {
+		logging.SetLevel(logging.CRITICAL, category)
+		return logger
+	}
+	logging.SetLevel(logging.INFO, category)
+
+	return logger
+}
--- logging/uuid.go
+++ logging/uuid.go
@@ -1,6 +1,8 @@
 package logging
 
-import uuid "github.com/satori/go.uuid"
+import (
+	uuid "github.com/satori/go.uuid"
+)
 
 // GenUUIDv4 generates a new universal user ID (UUID) version 4
 func GenUUIDv4() string {
