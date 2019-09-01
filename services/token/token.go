0c6a5760c5a94fcc56781433dc7237785f963b50
Mourad Kessas <mourad.kessas@9spokes.com>
2019-09-01 12:11:43 +1000

    Updated response type for GetConnections() to struct

--- services/token/token.go
+++ services/token/token.go
@@ -5,10 +5,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/globalsign/mgo/bson"
 )
 
+// Connection is a short-form connection object as returned by the token service, it excludes sensitive info and is meant as a summary
+type Connection struct {
+	ID       bson.ObjectId `bson:"_id" json:"id"`
+	Created  time.Time     `json:"created"`
+	Modified time.Time     `json:"modified"`
+	OSP      string        `json:"osp"`
+	Status   string        `json:"status"`
+}
+
 //GetConnections returns a list of documents from the Token service that match the criteria set forth in "filter"
-func GetConnections(url, filter, limit, selector string) ([]Document, error) {
+func GetConnections(url, filter, limit, selector string) ([]Connection, error) {
 
 	res, err := http.Get(fmt.Sprintf("%s/connections?filter=%s&limit=%s&selector=%s",
 		url,
@@ -29,10 +41,17 @@ func GetConnections(url, filter, limit, selector string) ([]Document, error) {
 		return nil, fmt.Errorf("received a non-OK response from token service: [%s] %s", res.Status, body)
 	}
 
-	connections := make([]Document, 0)
-	if err := json.Unmarshal(body, &connections); err != nil {
-		return nil, fmt.Errorf("while unmarshalling messaging: %s", err.Error())
+	var ret struct {
+		Status  string `json:"status"`
+		Details struct {
+			Connections []Connection `json:"connections"`
+		} `json:"details"`
+		Message string `json:"message"`
 	}
 
-	return connections, nil
+	if err := json.Unmarshal(body, &ret); err != nil {
+		return nil, fmt.Errorf("while unmarshalling message: %s", err.Error())
+	}
+
+	return ret.Details.Connections, nil
 }
