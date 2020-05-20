d8bece3d993d2e68fdd634a812aef217f987585b
Mourad Kessas <mourad.kessas@9spokes.com>
2020-05-20 18:30:01 +1000

    Updated UUIDv4

--- misc/uuid.go
+++ misc/uuid.go
@@ -3,15 +3,12 @@ package misc
 import (
 	"regexp"
 
-	uuid "github.com/satori/go.uuid"
+	"github.com/satori/go.uuid"
 )
 
 // GenUUIDv4 generates a new universal user ID (UUID) version 4
 func GenUUIDv4() string {
-	id, err := uuid.NewV4()
-	if err != nil {
-		panic(err)
-	}
+	id := uuid.NewV4()
 	return id.String()
 }
 
