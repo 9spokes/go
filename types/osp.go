b3672d566079a61218818f4ce0727e71c3cd4596
Mourad Kessas <mourad.kessas@9spokes.com>
2020-06-05 17:00:41 +1000

    Added tile type

--- types/osp.go
+++ types/osp.go
@@ -2,8 +2,14 @@ package types
 
 //OSP is an Online Service Provider definition
 type OSP struct {
-	Name                 string            `json:"osp"`
-	Credentials          map[string]string `json:"credentials"`
+	Name        string            `json:"osp"`
+	Credentials map[string]string `json:"credentials"`
+	Tiles       []OSPTile         `json:"tiles"`
+}
+
+// OSPTile is a tile definition for an OSP
+type OSPTile struct {
+	Name                 string `json:"name'`
 	NotificationSettings []struct {
 		Metric string   `json:"metric"`
 		Period []string `json:"period"`
