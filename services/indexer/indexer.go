73ee968051ddea3ded02e4facb21ee0bb91c1141
Mourad Kessas <mourad.kessas@9spokes.com>
2020-06-20 21:25:41 +1000

    Added indexer update method

--- services/indexer/indexer.go
+++ services/indexer/indexer.go
@@ -18,9 +18,9 @@ type Context struct {
 }
 
 // GetIndex returns a connection by ID from the designated indexer service instance
-func (ctx Context) GetIndex(company, osp, datasource string) (*types.IndexerDatasource, error) {
+func (ctx Context) GetIndex(company, osp, datasource, cycle string) (*types.IndexerDatasource, error) {
 
-	url := fmt.Sprintf("%s/%s/%s/%s", ctx.URL, company, osp, datasource)
+	url := fmt.Sprintf("%s/%s/%s/%s?cycle=%s", ctx.URL, company, osp, datasource, cycle)
 
 	if ctx.Logger != nil {
 		ctx.Logger.Debugf("Invoking Indexer service at: %s", url)
@@ -67,4 +67,56 @@ func (ctx Context) GetIndex(company, osp, datasource string) (*types.IndexerData
 	}
 
 	return &parsed.Details, nil
+}
+
+// UpdateIndex updates an entry with the data provided
+func (ctx Context) UpdateIndex(company, osp, datasource, cycle, index, outcome string, ok, retry bool) error {
+
+	url := fmt.Sprintf("%s/%s/%s/%s?cycle=%s&index=%s", ctx.URL, company, osp, datasource, cycle, index)
+
+	if ctx.Logger != nil {
+		ctx.Logger.Debugf("Invoking Indexer service at: %s", url)
+	}
+
+	response, err := http.Request{
+		URL: url,
+		Authentication: http.Authentication{
+			Scheme:   "basic",
+			Username: ctx.ClientID,
+			Password: ctx.ClientSecret,
+		},
+		ContentType: "application/x-www-form-urlencoded",
+		Body:        []byte(fmt.Sprintf("cycle=%s&index=%s", cycle, index)),
+	}.Post()
+
+	if err != nil {
+		e := fmt.Sprintf("Error invoking Indexer service at: %s: %s", url, err.Error())
+		if ctx.Logger != nil {
+			ctx.Logger.Error(e)
+		}
+		return fmt.Errorf(e)
+	}
+
+	var parsed struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+	}
+
+	if json.Unmarshal(response.Body, &parsed); err != nil {
+		e := fmt.Sprintf("Error parsing response from Indexer service: %s", err.Error())
+		if ctx.Logger != nil {
+			ctx.Logger.Error(e)
+		}
+		return fmt.Errorf(e)
+	}
+
+	if parsed.Status != "ok" {
+		e := fmt.Sprintf("Non-OK response received from Token service: %s", parsed.Message)
+		if ctx.Logger != nil {
+			ctx.Logger.Error(e)
+		}
+		return fmt.Errorf(e)
+	}
+
+	return nil
 }
