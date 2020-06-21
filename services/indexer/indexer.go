2d221bc49b41ac7dded577d36e384904b1ebdba5
Mourad Kessas <mourad.kessas@9spokes.com>
2020-06-21 13:45:11 +1000

    Added indexer update method

--- services/indexer/indexer.go
+++ services/indexer/indexer.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"time"
 
 	"github.com/9spokes/go/http"
 	"github.com/9spokes/go/types"
@@ -20,6 +21,15 @@ type Context struct {
 
 // GetIndex returns a connection by ID from the designated indexer service instance
 func (ctx Context) GetIndex(company, osp, datasource, cycle string) (*types.IndexerDatasource, error) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			err := fmt.Sprintf("%s", r)
+			if ctx.Logger != nil {
+				ctx.Logger.Errorf("an error occured parsing the response from the Indexer service: %s", err)
+			}
+		}
+	}()
 
 	url := fmt.Sprintf("%s/%s/%s/%s?cycle=%s", ctx.URL, company, osp, datasource, cycle)
 
@@ -38,11 +48,7 @@ func (ctx Context) GetIndex(company, osp, datasource, cycle string) (*types.Inde
 	}.Get()
 
 	if err != nil {
-		e := fmt.Sprintf("Error invoking Indexer service at: %s: %s", url, err.Error())
-		if ctx.Logger != nil {
-			ctx.Logger.Error(e)
-		}
-		return nil, fmt.Errorf(e)
+		return nil, fmt.Errorf("error invoking Indexer service at: %s: %s", url, err.Error())
 	}
 
 	var parsed struct {
@@ -51,22 +57,45 @@ func (ctx Context) GetIndex(company, osp, datasource, cycle string) (*types.Inde
 		Details types.IndexerDatasource `json:"details"`
 	}
 
-	if json.Unmarshal(response.Body, &parsed); err != nil {
-		e := fmt.Sprintf("Error parsing response from Indexer service: %s", err.Error())
-		if ctx.Logger != nil {
-			ctx.Logger.Error(e)
-		}
-		return nil, fmt.Errorf(e)
+	if err := json.Unmarshal(response.Body, &parsed); err != nil {
+		return nil, fmt.Errorf("error parsing response from Indexer service: %s", err.Error())
 	}
 
 	if parsed.Status != "ok" {
-		e := fmt.Sprintf("Non-OK response received from Token service: %s", parsed.Message)
-		if ctx.Logger != nil {
-			ctx.Logger.Error(e)
-		}
-		return nil, fmt.Errorf(e)
+		return nil, fmt.Errorf("non-OK response received from Indexer service: %s", parsed.Message)
 	}
 
+	switch parsed.Details.Type {
+	case "rolling":
+		data := make([]types.IndexerDatasourceRolling, len(parsed.Details.Data.([]interface{})))
+
+		for i, e := range parsed.Details.Data.([]interface{}) {
+			updated, _ := time.Parse(time.RFC3339, e.(map[string]interface{})["updated"].(string))
+			data[i] = types.IndexerDatasourceRolling{
+				Index:   e.(map[string]interface{})["index"].(string),
+				Outcome: e.(map[string]interface{})["outcome"].(string),
+				Period:  e.(map[string]interface{})["period"].(string),
+				Retry:   e.(map[string]interface{})["retry"].(bool),
+				Status:  e.(map[string]interface{})["status"].(string),
+				Updated: updated,
+			}
+		}
+		parsed.Details.Data = data
+
+	case "absolute":
+		e := parsed.Details.Data.(interface{})
+		updated, _ := time.Parse(time.RFC3339, e.(map[string]interface{})["updated"].(string))
+		expires, _ := time.Parse(time.RFC3339, e.(map[string]interface{})["expires"].(string))
+
+		parsed.Details.Data = types.IndexerDatasourceAbsolute{
+			Index:   e.(map[string]interface{})["index"].(string),
+			Outcome: e.(map[string]interface{})["outcome"].(string),
+			Expires: expires,
+			Retry:   e.(map[string]interface{})["retry"].(bool),
+			Status:  e.(map[string]interface{})["status"].(string),
+			Updated: updated,
+		}
+	}
 	return &parsed.Details, nil
 }
 
@@ -100,12 +129,8 @@ func (ctx Context) UpdateIndex(company, osp, datasource, cycle, index, outcome s
 		Body:        []byte(params.Encode()),
 	}.Put()
 
-	if err != nil {
-		e := fmt.Sprintf("Error invoking Indexer service at: %s: [%d] %s", location, response.StatusCode, err.Error())
-		if ctx.Logger != nil {
-			ctx.Logger.Error(e)
-		}
-		return fmt.Errorf(e)
+	if err != nil && response.StatusCode < 399 {
+		return fmt.Errorf("error invoking Indexer service at: %s: [%d] %s", location, response.StatusCode, err.Error())
 	}
 
 	var parsed struct {
@@ -113,20 +138,12 @@ func (ctx Context) UpdateIndex(company, osp, datasource, cycle, index, outcome s
 		Message string `json:"message"`
 	}
 
-	if json.Unmarshal(response.Body, &parsed); err != nil {
-		e := fmt.Sprintf("Error parsing response from Indexer service: %s", err.Error())
-		if ctx.Logger != nil {
-			ctx.Logger.Error(e)
-		}
-		return fmt.Errorf(e)
+	if err := json.Unmarshal(response.Body, &parsed); err != nil {
+		return fmt.Errorf("error parsing response from Indexer service: %s", err.Error())
 	}
 
 	if parsed.Status != "ok" {
-		e := fmt.Sprintf("Non-OK response received from Indexer service: %s", parsed.Message)
-		if ctx.Logger != nil {
-			ctx.Logger.Error(e)
-		}
-		return fmt.Errorf(e)
+		return fmt.Errorf("non-OK response received from Indexer service: %s", parsed.Message)
 	}
 
 	return nil
