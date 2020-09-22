49d0a31ba54d240d895953cc379ad314a80b208b
Mourad <mourad@kessas.com>
2020-09-22 19:10:02 +1000

    Modified company structure

--- types/company.go
+++ types/company.go
@@ -6,29 +6,29 @@ import "time"
 type Company struct {
 	ID         string `json:"id" bson:"company"`
 	Name       string `json:"name" bson:"name"`
-	Industry   string `json:"industry"`
+	Industry   string `json:"industry,omitempty"`
 	Industries []struct {
 		Code        string `json:"code"`
 		Description string `json:"description"`
-	} `json:"industries"`
+	} `json:"industries,omitempty"`
 	Location struct {
 		Country  string `json:"country"`
 		Timezone int    `json:"timezone"`
-	} `json:"location"`
+	} `json:"location,omitempty"`
 	Type struct {
 		Code        string `json:"code"`
 		Description string `json:"description"`
-	} `json:"type"`
+	} `json:"type,omitempty"`
 	Entity            string                 `json:"entity,omitempty"`
 	Phone             string                 `json:"phone"`
 	Users             []string               `json:"users"`
-	TimeZoneOffset    int64                  `json:"timeZoneOffset"`
-	WorkingHoursStart string                 `json:"workingHoursStart"`
-	WorkingHoursEnd   string                 `json:"workingHoursEnd"`
+	TimeZoneOffset    int64                  `json:"timeZoneOffset,omitempty"`
+	WorkingHoursStart string                 `json:"workingHoursStart,omitempty"`
+	WorkingHoursEnd   string                 `json:"workingHoursEnd,omitempty"`
 	Created           time.Time              `json:"created"`
 	Updated           time.Time              `json:"updated"`
-	Details           CompanyDetails         `json:"details"`
-	Extras            map[string]interface{} `json:"extras"`
+	Details           CompanyDetails         `json:"details,omitempty"`
+	Extras            map[string]interface{} `json:"extras,omitempty"`
 }
 
 // CompanyAddress represents a company address
