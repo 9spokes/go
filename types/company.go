fa1598045bf5ebf49d42030fd50c46e04e697394
Mourad <mourad@kessas.com>
2020-10-21 13:38:45 +1000

    Added BusinessHours

--- types/company.go
+++ types/company.go
@@ -4,23 +4,30 @@ import "time"
 
 //Company is a struct that defines a 9 Spokes company entity
 type Company struct {
-	ID                string                 `json:"id" bson:"company"`
-	Name              string                 `json:"name" bson:"name"`
-	Industry          string                 `json:"industry,omitempty" bson:"industry,omitempty"`
-	Industries        []CompanyRecord        `json:"industries"`
-	Location          CompanyLocation        `json:"location"`
-	Type              CompanyRecord          `json:"type"`
-	Status            CompanyRecord          `json:"status"`
-	Entity            string                 `json:"entity,omitempty" bson:"entity,omitempty"`
-	Phone             string                 `json:"phone,omitempty" bson:"phone,omitempty"`
-	Users             []string               `json:"users"`
-	TimeZoneOffset    int64                  `json:"timeZoneOffset,omitempty" bson:"timeZoneOffset,omitempty"`
-	WorkingHoursStart string                 `json:"workingHoursStart,omitempty" bson:"workingHoursStart,omitempty"`
-	WorkingHoursEnd   string                 `json:"workingHoursEnd,omitempty" bson:"workingHoursEnd,omitempty"`
-	Created           time.Time              `json:"created"`
-	Updated           time.Time              `json:"updated"`
-	Details           *CompanyDetails        `json:"details,omitempty" bson:"details,omitempty"`
-	Extras            map[string]interface{} `json:"extras,omitempty" bson:"extras,omitempty"`
+	ID             string                 `json:"id" bson:"company"`
+	Name           string                 `json:"name" bson:"name"`
+	Industry       string                 `json:"industry,omitempty" bson:"industry,omitempty"`
+	Industries     []CompanyRecord        `json:"industries"`
+	Location       CompanyLocation        `json:"location"`
+	Type           CompanyRecord          `json:"type"`
+	Status         CompanyRecord          `json:"status"`
+	Entity         string                 `json:"entity,omitempty" bson:"entity,omitempty"`
+	Phone          string                 `json:"phone,omitempty" bson:"phone,omitempty"`
+	Users          []string               `json:"users"`
+	TimeZoneOffset int64                  `json:"timeZoneOffset,omitempty" bson:"timeZoneOffset,omitempty"`
+	BusinessHours  CompanyBusinessHours   `json:"businessHours"`
+	Created        time.Time              `json:"created"`
+	Updated        time.Time              `json:"updated"`
+	Details        *CompanyDetails        `json:"details,omitempty" bson:"details,omitempty"`
+	Extras         map[string]interface{} `json:"extras,omitempty" bson:"extras,omitempty"`
+}
+
+// CompanyBusinessHours represent the working days & hours for a company
+type CompanyBusinessHours struct {
+	DaysFrom  int `json:"daysFrom"`
+	DaysTo    int `json:"daysTo"`
+	HoursFrom int `json:"hoursFrom"`
+	HoursTo   int `json:"hoursTo"`
 }
 
 // CompanyLocation contains the company's locality details
