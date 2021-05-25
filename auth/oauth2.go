2f4038e96397d24c2aec436324a30368ecf106bb
Mathew Tonkin Henwood <mathew.henwood@9spokes.com>
2021-05-26 10:07:10 +1200

    Extract and combine repeated code in oauth methods
    Remove repeated code from Authorize() and Refresh(), both now use
    oauthRequest()

--- auth/oauth2.go
+++ auth/oauth2.go
@@ -29,28 +29,12 @@ type Options struct {
 	DataInQuery bool `default:"false"`
 }
 
-// Authorize implements an OAuth2 authorization using the parameters defined in the OAuth2 struct
-func (params OAuth2) Authorize(opt Options) (map[string]interface{}, error) {
-
+func (params OAuth2) oauthRequest(opt Options, data url.Values) (map[string]interface{}, error) {
 	if params.ClientID == "" {
 		return nil, fmt.Errorf("client_id cannot be empty")
 	}
 
-	if params.Code == "" {
-		return nil, fmt.Errorf("the authorization code is missing")
-	}
-
 	var auth string
-
-	data := url.Values{}
-	data.Set("grant_type", "authorization_code")
-	data.Set("code", params.Code)
-	data.Set("redirect_uri", params.RedirectURI)
-
-	for k, v := range params.Extras {
-		data.Set(k, v)
-	}
-
 	if opt.AuthInHeader {
 		auth = "Basic " + base64.StdEncoding.EncodeToString([]byte(params.ClientID+":"+params.ClientSecret))
 	} else {
@@ -84,10 +68,6 @@ func (params OAuth2) Authorize(opt Options) (map[string]interface{}, error) {
 		return nil, fmt.Errorf("failed to create an HTTP client: %s", err.Error())
 	}
 
-	for k, v := range params.Headers {
-		request.Header.Set(k, v)
-	}
-
 	if opt.AuthInHeader {
 		request.Header.Set("Authorization", auth)
 	}
@@ -97,10 +77,14 @@ func (params OAuth2) Authorize(opt Options) (map[string]interface{}, error) {
 		request.Header.Set("Content-type", "application/x-www-form-urlencoded")
 	}
 
+	for k, v := range params.Headers {
+		request.Header.Set(k, v)
+	}
+
 	client :=  &http.Client{}
 	if params.Client != nil {
 		client = params.Client
-	} 
+	}
 
 	response, err := client.Do(request)
 	if err != nil {
@@ -143,6 +127,21 @@ func (params OAuth2) Authorize(opt Options) (map[string]interface{}, error) {
 	return nil, fmt.Errorf("could not determine content type encoding from response")
 }
 
+// Authorize implements an OAuth2 authorization using the parameters defined in the OAuth2 struct
+func (params OAuth2) Authorize(opt Options) (map[string]interface{}, error) {
+
+	if params.Code == "" {
+		return nil, fmt.Errorf("the authorization code is missing")
+	}
+
+	data := url.Values{}
+	data.Set("grant_type", "authorization_code")
+	data.Set("code", params.Code)
+	data.Set("redirect_uri", params.RedirectURI)
+
+	return params.oauthRequest(opt, data)
+}
+
 // Refresh implements an OAuth2 token refresh methods.  Parameters are sent via the OAuth2 struct
 func (params OAuth2) Refresh(opt Options) (map[string]interface{}, error) {
 	
@@ -150,100 +149,9 @@ func (params OAuth2) Refresh(opt Options) (map[string]interface{}, error) {
 		return nil, fmt.Errorf("the refresh token is missing")
 	}
 
-	var auth string
-
 	data := url.Values{}
 	data.Set("grant_type", "refresh_token")
 	data.Set("refresh_token", params.RefreshToken)
 
-	for k, v := range params.Extras {
-		data.Set(k, v)
-	}
-
-	if opt.AuthInHeader {
-		auth = "Basic " + base64.StdEncoding.EncodeToString([]byte(params.ClientID+":"+params.ClientSecret))
-	} else {
-		data.Set("client_id", params.ClientID)
-		if !opt.DataInQuery && params.ClientSecret != "" {
-			data.Set("client_secret", params.ClientSecret)
-		}
-	}
-
-	var body string
-	requestUrl := params.TokenEndpoint
-
-	if opt.DataInQuery {
-		u, err := url.Parse(params.TokenEndpoint)
-		if err != nil {
-			return nil, err
-		}
-		rawQuery := u.RawQuery
-		if rawQuery != "" {
-			rawQuery += "&"
-		}
-		rawQuery += data.Encode()
-		u.RawQuery = rawQuery
-		requestUrl = u.String()
-	} else {
-		body = data.Encode()
-	}
-
-	request, err := http.NewRequest("POST", requestUrl, strings.NewReader(body))
-	if err != nil {
-		return nil, fmt.Errorf("failed to create an HTTP client: %s", err.Error())
-	}
-
-	if opt.AuthInHeader {
-		request.Header.Set("Authorization", auth)
-	}
-
-	request.Header.Set("Accept", "application/json")
-	if !opt.DataInQuery {
-		request.Header.Set("Content-type", "application/x-www-form-urlencoded")
-	}
-
-	client :=  &http.Client{}
-	if params.Client != nil {
-		client = params.Client
-	} 
-
-	response, err := client.Do(request)
-	if err != nil {
-		return nil, fmt.Errorf("error while connecting to %s: %s", params.TokenEndpoint, err.Error())
-	}
-
-	defer response.Body.Close()
-
-	resp, _ := ioutil.ReadAll(response.Body)
-
-	if response.StatusCode != 200 {
-		return nil, fmt.Errorf("non-OK response received: %s", resp)
-	}
-
-	if response.Header["Content-Type"] == nil {
-		return nil, fmt.Errorf("content-type header missing in response: %s", resp)
-	}
-
-	contentType := response.Header["Content-Type"][0]
-
-	if strings.Contains(contentType, "application/json") {
-		var parsed map[string]interface{}
-		if err = json.Unmarshal(resp, &parsed); err != nil {
-			return nil, fmt.Errorf("failed to deserialise the response: %s (%s)", resp, err.Error())
-		}
-		return parsed, nil
-	}
-
-	if strings.Contains(contentType, "application/x-www-form-urlencoded") || strings.Contains(contentType, "text/html") {
-
-		m, _ := url.ParseQuery(string(resp))
-		ret := make(map[string]interface{})
-		for k, v := range m {
-			ret[k] = v[0]
-		}
-
-		return ret, nil
-	}
-
-	return nil, fmt.Errorf("could not determine content type encoding from response")
+	return params.oauthRequest(opt, data)
 }
