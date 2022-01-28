a77afa15eaf6e84d995bce347dde5a28273e53f0
Mathew Tonkin Henwood <mathew.henwood@9spokes.com>
2022-01-28 14:15:11 +1300

    Add request method, url, headers to recoverer log
    Should be helpful information for debugging

--- middleware/recoverer/recoverer.go
+++ middleware/recoverer/recoverer.go
@@ -11,8 +11,14 @@ func Recoverer(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil && err != http.ErrAbortHandler {
-				logging.Errorf("Unhandled exception: %s %s", err, debug.Stack())
-				w.WriteHeader(http.StatusInternalServerError)
+				if r != nil {
+					logging.Errorf("Unhandled panic: %v, \nstack: %s \nrequest: %s %v, \nheaders: %v", err, debug.Stack(), r.Method, r.URL, r.Header)
+				} else {
+					logging.Errorf("Unhandled panic with nil http request object: %s", debug.Stack())
+				}
+				if w != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+				}
 			}
 		}()
 
