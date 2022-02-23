70d33c4c4923051b63f1509d7b670d8b66a54ef5
Karl D'Lima <karl.dlima@9spokes.com>
2022-02-24 12:01:41 +1300

    accounts type

---
+++ types/account.go
@@ -0,0 +1,12 @@
+package types
+
+type Account struct {
+	AccountID       string  `json:"account_id,omitempty"`
+	AccountActive   bool    `json:"account_active,omitempty"`
+	AccountBalance  float64 `json:"account_balance,omitempty"`
+	AccountCurrency string  `json:"account_currency,omitempty"`
+	AccountName     string  `json:"account_name,omitempty"`
+	AccountType     string  `json:"account_type,omitempty"`
+	AccountCategory string  `json:"account_category,omitempty"`
+	AccountStatus   string  `json:"account_status,omitempty"`
+}
