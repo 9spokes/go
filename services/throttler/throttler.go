cf1a2d41d8d480863f4f17cd529d0734bb14830a
Adrian Ionica <adrian.ionica@9spokes.com>
2022-05-06 15:54:15 +1200

    BUG-26547: opts to opt

--- services/throttler/throttler.go
+++ services/throttler/throttler.go
@@ -12,9 +12,13 @@ import (
 
 // Context represents a connection object into the token service
 type Context struct {
-	URL           string
-	ClientID      string
-	ClientSecret  string
+	URL          string
+	ClientID     string
+	ClientSecret string
+}
+
+type Options struct {
+	Retries       int
 	CorrelationID string
 }
 
@@ -22,26 +26,17 @@ var (
 	ErrTooManyRequests = errors.New("failed to acquire token")
 )
 
-func (ctx Context) WithCID(correlationID string) Context {
-	return Context{
-		URL:           ctx.URL,
-		ClientID:      ctx.ClientID,
-		ClientSecret:  ctx.ClientSecret,
-		CorrelationID: correlationID,
-	}
-}
-
 // GetToken is a helper function that retrieves a token from the OSP rate limiting service `etl-throttler`.
 //
 // You must call this function before initiating any outbound connection which enforces a rate limiting policy.
 //
 // The function will block until a valid token is returned from the throttling service hence you may wish to implement a timeout
-func (ctx Context) GetToken(osp string, retries int) error {
+func (ctx Context) GetToken(osp string, opt Options) error {
 
-	logging.Debugf("[CorrID:%s][%s] Getting rate-limiting token", ctx.CorrelationID, osp)
+	logging.Debugf("[CorrID:%s][%s] Getting rate-limiting token", opt.CorrelationID, osp)
 
 	url := fmt.Sprintf("%s/token/%s", ctx.URL, osp)
-	logging.Debugf("[CorrID:%s][%s] URL is: %s", ctx.CorrelationID, osp, ctx.URL)
+	logging.Debugf("[CorrID:%s][%s] URL is: %s", opt.CorrelationID, osp, ctx.URL)
 
 	request := http.Request{
 		URL: url,
@@ -51,31 +46,31 @@ func (ctx Context) GetToken(osp string, retries int) error {
 			Password: ctx.ClientSecret,
 		},
 		Headers: map[string]string{
-			"x-correlation-id": ctx.CorrelationID,
+			"x-correlation-id": opt.CorrelationID,
 		},
 		ContentType: "application/json",
 	}
 
-	for i := 0; i < retries; i++ {
-		logging.Debugf("[CorrID:%s][%s] Attempt #%d", ctx.CorrelationID, osp, i+1)
+	for i := 0; i < opt.Retries; i++ {
+		logging.Debugf("[CorrID:%s][%s] Attempt #%d", opt.CorrelationID, osp, i+1)
 		response, err := request.Get()
 		if err != nil {
 			return fmt.Errorf("failed to issue request: %w", err)
 		}
 
 		if response.StatusCode == 200 {
-			logging.Debugf("[CorrID:%s][%s] Successfully acquired rate-limiting token", ctx.CorrelationID, osp)
+			logging.Debugf("[CorrID:%s][%s] Successfully acquired rate-limiting token", opt.CorrelationID, osp)
 			return nil
 		}
 
 		if response.StatusCode != 429 {
-			logging.Debugf("[CorrID:%s][%s] Unexpected response from throttling service: %d", ctx.CorrelationID, osp, response.StatusCode)
+			logging.Debugf("[CorrID:%s][%s] Unexpected response from throttling service: %d", opt.CorrelationID, osp, response.StatusCode)
 			return fmt.Errorf("unexpected response from throttling service: %d", response.StatusCode)
 		}
 
 		time.Sleep(5 * time.Second)
 	}
 
-	logging.Errorf("[CorrID:%s][%s] Failed to acquire rate-limtiing token after %d attempts", ctx.CorrelationID, osp, retries)
+	logging.Errorf("[CorrID:%s][%s] Failed to acquire rate-limtiing token after %d attempts", opt.CorrelationID, osp, opt.Retries)
 	return ErrTooManyRequests
 }
