4274560983943d41b15f9cee032221571b6367a1
root <root@LAPTOP-5O68PHNR.localdomain>
2022-06-20 16:59:13 +1200

    Added new index type for the new connection based ETL

--- services/indexer/types.go
+++ services/indexer/types.go
@@ -40,6 +40,37 @@ type Index struct {
 	Data         interface{} `json:"data,omitempty"`
 }
 
+type NewIndex struct {
+	Count      int64        `json:"count"`
+	Cycle      string       `json:"cycle"`
+	Connection string       `json:"connection,omitempty"`
+	Datasource string       `json:"datasource"`
+	Webhooks   bool         `json:"webhooks,omitempty"`
+	Notify     bool         `json:"notify,omitempty"`
+	OSP        string       `json:"osp"`
+	Status     string       `json:"status,omitempty"`
+	Storage    string       `json:"storage"`
+	Type       string       `json:"type"`
+	Data       []IndexEntry `json:"data,omitempty"`
+}
+
+type IndexEntry struct {
+	Period  string    `json:"period"`
+	Status  string    `json:"status"`
+	Retry   bool      `json:"retry"`
+	Updated time.Time `json:"updated,omitempty"`
+	Outcome string    `json:"outcome"`
+	Index   string    `json:"index"`
+	Cycle   string    `json:"cycle"`
+}
+
+type UpdateBody struct {
+	Connection string       `json:"connection"`
+	Datasource string       `json:"datasource"`
+	Cycle      string       `json:"cycle"`
+	Data       []IndexEntry `json:"data"`
+}
+
 // IndexStatus is the response type for getting index status
 type IndexStatus struct {
 	Datasource  string    `json:"datasource"`
