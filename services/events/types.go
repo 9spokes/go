cc355be2ba7782d0fdaf6706d6ed72da083b1353
Adrian Ionica <Adrian.Ionica@9spokes.com>
2022-11-02 06:05:03 +0000

    Merged PR 15065: TASK-28534: event updates
    - change timestamp type from `int64` to `time.Time`
    - change `Post` parameter type from `map[string]interface{}` to `Event`
    - move event related types in the the `events` package
    
    Related work items: #28534

--- services/events/types.go
+++ services/events/types.go
@@ -1,18 +1,20 @@
-package types
+package events
 
-//Event is a struct containing a generic event created by any 9 Spokes service
+import "time"
+
+// Event is a struct containing a generic event created by any 9 Spokes service
 type Event struct {
 	Action    string                 `json:"action"`
 	Service   string                 `json:"service"`
-	Timestamp int64                  `json:"timestamp"`
+	Timestamp time.Time              `json:"timestamp"`
 	User      string                 `json:"user"`
-	Email     string                 `json:"email"`
-	Session   string                 `json:"session"`
+	Email     string                 `json:"email,omitempty"`
+	Session   string                 `json:"session,omitempty"`
 	Data      map[string]interface{} `json:"data"`
 	Channel   string                 `json:"channel"`
 }
 
-//HandlerConfig is entry in the handlers.yaml configuration file, used to supply runtime options to various event handlers
+// HandlerConfig is entry in the handlers.yaml configuration file, used to supply runtime options to various event handlers
 type HandlerConfig struct {
 	Name    string                 `yaml:"name"`
 	Enabled bool                   `yaml:"enabled"`
