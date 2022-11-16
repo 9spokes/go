4bded4bb911ce5bf1a30f1989e639d847b50d2e9
Melvin Narayan <melvin.narayan@9spokes.com>
2022-11-16 03:46:58 +0000

    Merged PR 15168: add bank account type
    add bank account type
    fix transactiondirection enums
    
    Related work items: #28286

---
+++ types/banking/bankAccount.go
@@ -0,0 +1,10 @@
+package banking
+
+type BankAccount struct {
+	Id           string          `json:"accountId" bson:"account_id"`
+	Type         BankAccountType `json:"type" bson:"type"`
+	Number       string          `json:"accountNumber" bson:"account_number"`
+	NumberMasked string          `json:"accountNumberMasked" bson:"account_number_masked"`
+	DisplayName  string          `json:"displayName" bson:"display_name"`
+	Currency     string          `json:"currency" bson:"currency"`
+}
