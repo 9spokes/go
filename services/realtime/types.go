418b7cf5448003bf1b36228f02288ff5cb6f3b4e
Adrian Ionica <adrian.ionica@9spokes.com>
2022-11-24 16:41:16 +1300

    BUG-28268: capture the current as well as the available balance in the BankingAccount struct

--- services/realtime/types.go
+++ services/realtime/types.go
@@ -1,20 +1,21 @@
 package realtime
 
 type BankingAccount struct {
-	AccountID               string  `json:"account_id,omitempty"`
-	AccountActive           bool    `json:"account_active,omitempty"`
-	AccountBalance          float64 `json:"account_balance,omitempty"`
-	AccountCurrency         string  `json:"account_currency,omitempty"`
-	AccountName             string  `json:"account_name,omitempty"`
-	AccountType             string  `json:"account_type,omitempty"`
-	AccountCategory         string  `json:"account_category,omitempty"`
-	AccountStatus           string  `json:"account_status,omitempty"`
-	AccountNumber           string  `json:"account_number,omitempty"`
-	AccountNumberDisplay    string  `json:"account_number_display,omitempty"`
-	AccountNextPayDate      string  `json:"account_next_pay_date,omitempty"`
-	AccountMinPayAmount     float64 `json:"account_min_pay_amount,omitempty"`
-	AccountLastPayAmount    float64 `json:"account_last_pay_amount,omitempty"`
-	AccountCashAdvanceLimit float64 `json:"account_cash_advance_limit,omitempty"`
-	AccountPointsAccrued    float64 `json:"account_points_accrued,omitempty"`
-	AccountRewardsBalance   float64 `json:"account_rewards_balance,omitempty"`
+	ID               string  `json:"account_id,omitempty"`
+	Active           bool    `json:"account_active,omitempty"`
+	CurrentBalance   float64 `json:"account_current_balance,omitempty"`
+	AvailableBalance float64 `json:"account_available_balance,omitempty"`
+	Currency         string  `json:"account_currency,omitempty"`
+	Name             string  `json:"account_name,omitempty"`
+	Type             string  `json:"account_type,omitempty"`
+	Category         string  `json:"account_category,omitempty"`
+	Status           string  `json:"account_status,omitempty"`
+	Number           string  `json:"account_number,omitempty"`
+	NumberDisplay    string  `json:"account_number_display,omitempty"`
+	NextPayDate      string  `json:"account_next_pay_date,omitempty"`
+	MinPayAmount     float64 `json:"account_min_pay_amount,omitempty"`
+	LastPayAmount    float64 `json:"account_last_pay_amount,omitempty"`
+	CashAdvanceLimit float64 `json:"account_cash_advance_limit,omitempty"`
+	PointsAccrued    float64 `json:"account_points_accrued,omitempty"`
+	RewardsBalance   float64 `json:"account_rewards_balance,omitempty"`
 }
