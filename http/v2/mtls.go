3f4d6f581eff7b2b09206112b6ac002c4b20ec05
Adrian Ionica <adrian.ionica@9spokes.com>
2023-01-09 16:51:44 +1300

    BUG-28901: expose http v1 functionality in v2

---
+++ http/v2/mtls.go
@@ -0,0 +1,14 @@
+package http
+
+import (
+	"net/http"
+
+	v1 "github.com/9spokes/go/http"
+)
+
+type MTLSRequest v1.MTLSRequest
+
+// Creates a new secure HTTP client.
+func (req MTLSRequest) New() (*http.Client, error) {
+	return req.New()
+}
