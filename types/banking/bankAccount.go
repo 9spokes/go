cc396d11a57a061f5d10735ee9f50edb288122b4
Adrian Ionica <adrian.ionica@9spokes.com>
2023-02-08 17:28:08 +1300

    TASK-29230: include current and available balances

--- types/banking/bankAccount.go
+++ types/banking/bankAccount.go
@@ -1,10 +1,12 @@
 package banking
 
 type BankAccount struct {
-	Id           string          `json:"accountId" bson:"account_id"`
-	Type         BankAccountType `json:"type" bson:"type"`
-	Number       string          `json:"accountNumber" bson:"account_number"`
-	NumberMasked string          `json:"accountNumberMasked" bson:"account_number_masked"`
-	DisplayName  string          `json:"displayName" bson:"display_name"`
-	Currency     string          `json:"currency" bson:"currency"`
+	Id               string          `json:"accountId" bson:"account_id"`
+	Type             BankAccountType `json:"type" bson:"type"`
+	Number           string          `json:"accountNumber" bson:"account_number"`
+	NumberMasked     string          `json:"accountNumberMasked" bson:"account_number_masked"`
+	DisplayName      string          `json:"displayName" bson:"display_name"`
+	Currency         string          `json:"currency" bson:"currency"`
+	CurrentBalance   float64         `json:"currentBalance" bson:"currentBalance"`
+	AvailableBalance float64         `json:"availableBalance" bson:"availableBalance"`
 }
